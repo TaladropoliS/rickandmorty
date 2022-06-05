7718bb185b3618c4a30ea8ff6c9313055495b68c
Fernando Taladriz <fernando.taladriz@gmail.com>
2022-06-05 11:04:51 -0400

    first

---
+++ src/components/Character.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="col-md-8 col-lg-6 mx-auto">
+    <div class="card mb-3 text-success bg-transparent border-success rounded-3"
+         v-for="(personaje, i) in data" :key="i">
+      <div class="row p-2">
+        <div class="col-3 col-md-2">
+          <img :src="personaje.image"
+               class="img-fluid rounded-circle" alt="Rick Sanchez">
+        </div>
+        <div class="col-9 col-md-10 my-auto text-start">
+          <h4 class="card-title">{{ personaje.name }}</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "Character",
+    props: ['data'],
+    data() {
+      return {}
+    },
+
+  }
+</script>
+
+<style scoped>
+h4 {
+  text-shadow: 1px 1px #969696;
+}
+</style>
---
+++ src/views/RickAndMorty.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="rickandmorty min-vh-100 p-5 text-center">
+    <div class="d-inline-flex text-center p-3">
+      <h1 class="text-center text-light">Rick and Morty</h1>
+    </div>
+    <Character :data="listaPersonajes"/>
+  </div>
+
+</template>
+
+<script>
+  import Character from "@/components/Character";
+
+  export default {
+    name: "RickAndMorty",
+    components: {Character},
+    data() {
+      return {
+        listaPersonajes: []
+      }
+    },
+    methods: {
+      async llamarPersonajes() {
+        try {
+          const data = await fetch('https://rickandmortyapi.com/api/character/')
+          const personajes = await data.json()
+          this.listaPersonajes = personajes.results
+        } catch (error) {
+          console.warn(error)
+        }
+      },
+    },
+    created() {
+      this.llamarPersonajes()
+    }
+  }
+</script>
+
+<style scoped>
+.rickandmorty {
+  background-image: url("../assets/img/rickandmortyBackground01.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+</style>
